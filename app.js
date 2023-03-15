81a81b9c3ee6ed8f2e754c4584af6d2b16664a91
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-15 07:25:59 +0100

    using path and route in express

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 const Port = 3000;
+const path = require('path');
+const router = express.Router();
+
+// app.get('/', (req, res) => {
+//     res.send("Homepage! helloworld")
+// })
+// app.get('/about', (req,res) =>{
+//     res.send("About page Nice.")
+// })
+// app.get('/contact',(req, res)=>{
+//     res.send("Contact page nice.")
+// })
+
 
 app.get('/', (req, res) => {
-    res.send("Homepage! helloworld")
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname + '/about.html' ))
 })
+
+
 
 
 app.listen(Port, ()=> console.log(`Listening to port ${Port}`))
