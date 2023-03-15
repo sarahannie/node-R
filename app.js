aa10b62f3c0c1807e7bc39fd00d2ebc6608a8759
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-15 06:41:19 +0100

    install nodemon

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log(" hello World")
+const express = require("express");
+const app = express();
+const Port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Homepage! helloworld")
+})
+
+
+app.listen(Port, ()=> console.log(`Listening to port ${Port}`))
