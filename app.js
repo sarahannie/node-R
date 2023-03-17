b9eeb396b402bd68614d7c6932195471d82b4c93
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-17 07:17:46 +0100

    dynamic routing using pug

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const Port = 3000;
-const path = require('path');
 const router = express.Router();
+const Port = 3000;
 app.use(router);
 
-// app.get('/', (req, res) => {
-//     res.send("Homepage! helloworld")
-// })
-// app.get('/about', (req,res) =>{
-//     res.send("About page Nice.")
-// })
-// app.get('/contact',(req, res)=>{
-//     res.send("Contact page nice.")
-// })
+app.set("view engine","pug");
+app.set("views", path.join(__dirname,"views"))
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/about', (req,res) => {
-    res.sendFile(path.join(__dirname, 'about.html' ))
+router.get('/', (req, res)=>{
+    res.render("employee")
 })
 
+router.get('/about', (req, res)=>{
+    res.render("about",{title:"Hello", message: "How are you doing"})
+})
 
-router.get('/all', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
-
-
-
-
-
-app.listen(Port, ()=> console.log(`Listening to port ${Port}`))
+app.listen(process.env.port || Port, ()=>{
+console.log(`listen to port ${Port}`)
+})
