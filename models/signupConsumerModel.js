15920907cdfb91ef6f4855f2a6946b06c7e180d5
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-28 10:48:39 +0100

    done with useconsumer model

--- models/signupConsumerModel.js
+++ models/signupConsumerModel.js
@@ -1,5 +1,28 @@
 const mongoose = require("mongoose");
 
 const signupSchema = new mongoose.Schema({
-    
+    fullname:{
+        type:String,
+        trim:true,
+    },
+    email:{
+        type:String,
+        trim:true
+    },
+    gender:{
+        type:String,
+        trim:true
+    },
+    phone:{
+        type:Number,
+        trim:true
+    },
+    password:{
+        type:String,
+        trim:true
+    },
+    repassword:{
+        type:String,
+        trim:true
+    }
 })
--- routes/signupconsumerRouter.js
+++ routes/signupconsumerRouter.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Signup = require("../models/signupConsumerModel")
 
 router.get('/signupconsumer', (req, res)=>{
     res.render("signup-consumer")
+})
+
+
+
+router.post("/signupconsumer", async(req,res)=>{
+    try{
+        const signup = new Signup(req.body);
+        await signup.save()
+        res.redirect("/signin")
+    }catch(err){
+        res.status(400).render("signup-consumer")
+    }
 })
 
 
