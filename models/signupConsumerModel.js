204c807c121766a0734efa1856006a5f871a283c
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-28 10:25:40 +0100

    example done let start real work.

---
+++ models/signupConsumerModel.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const signupSchema = new mongoose.Schema({
+    
+})
--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -12,11 +12,22 @@ router.post("/register", async(req,res)=>{
     try{
         const register = new Register(req.body);
         await register.save()
-        res.redirect("/")
+        res.redirect("/students")
         console.log(req.body)
     }catch(err){
         // res.status(400).render("register")
         console.log(err)
+    }
+})
+
+router.get("/students",async(req,res)=>{
+    try{
+        let items = await Register.find();
+        // console.log(items)
+        res.render("students",{students:items})
+    }catch(err){
+        console.log(err);
+        res.send("failed to retrive student details")
     }
 })
 
