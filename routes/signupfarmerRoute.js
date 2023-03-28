baf7e04e6a8aabb16808314c3696d6974fcb7559
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-28 15:09:59 +0100

    starting routing

--- routes/signupfarmerRoute.js
+++ routes/signupfarmerRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const SignUP = require("../models/signupFarmer")
 
 
 router.get('/signupfarmer', (req,res)=>{
     res.render("signup-farmer")
 })
 
+// render.post("")
 module.exports = router;
