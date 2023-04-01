87d80f1ccb4b237d9aed04fbe30b828414f5f808
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-04-01 19:16:18 +0100

    index

--- public/js/AgricultureOffice/sidebar.js
+++ public/js/AgricultureOffice/sidebar.js
@@ -1,38 +1,47 @@
+// Wait for the DOM to load before running the code
 document.addEventListener("DOMContentLoaded", function(event) {
-   
-    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId)
-    
-    // Validate that all variables exist
-    if(toggle && nav && bodypd && headerpd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    nav.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
-    }
-    }
-    
-    showNavbar('header-toggle','nav-bar','body-pd','header')
-    
+
+    // Function to show/hide the navigation menu
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+      // Get the necessary elements by ID
+      const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId),
+        bodypd = document.getElementById(bodyId),
+        headerpd = document.getElementById(headerId);
+  
+      // Validate that all variables exist
+      if (toggle && nav && bodypd && headerpd) {
+        toggle.addEventListener('click', () => {
+          // Show/hide the navigation menu
+          nav.classList.toggle('show');
+          // Change the toggle icon
+          toggle.classList.toggle('bx-x');
+          // Add/remove padding to the body and header
+          bodypd.classList.toggle('body-pd');
+          headerpd.classList.toggle('body-pd');
+        });
+      }
+    };
+  
+    // Call the showNavbar function with the appropriate IDs
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+  
     /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
-    
-    function colorLink(){
-    if(linkColor){
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
+  
+    // Get all the navigation links
+    const linkColor = document.querySelectorAll('.nav_link');
+  
+    // Function to add/remove the active class on the clicked link
+    function colorLink() {
+      if (linkColor) {
+        linkColor.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+      }
     }
-    }
-    linkColor.forEach(l=> l.addEventListener('click', colorLink))
-    
-     // Your code to run since DOM is loaded and ready
-    });
+  
+    // Add a click event listener to each navigation link
+    linkColor.forEach(l => l.addEventListener('click', colorLink));
+  
+    // Your code to run since DOM is loaded and ready
+  });
+  
