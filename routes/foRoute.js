bdb449fb7d906e93b07acf54b0d6fd6f49de45e3
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-04-07 10:09:54 +0100

    class work commit

--- routes/foRoute.js
+++ routes/foRoute.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const connectEnsureLogin = require("connect-ensure-login")
+const connectEnsureLogin = require("connect-ensure-login");
+const User = require("../models/userModel")
 
 
-router.get('/fodash',connectEnsureLogin.ensureLoggedIn(), (req, res)=>{
-    res.render("foDash")
+router.get('/fodash', async(req, res)=>{
+    let farmerWard = req.user["ward"]
+    if(req.user.role == "fo"){
+        try{
+            let activeFarmers = await User.countDocuments({role: "uf", ward:farmerWard});
+            let newUrbanFarmers = await User.find({role: "uf", ward:farmerWard})
+
+            res.render("foDash", {   
+                urbanFarmers: newUrbanFarmers,
+                activeFarmers,
+                farmerWard
+                })
+        }catch(err){
+            console.log(err)
+            res.status(400).send("could not find farmers in the data base")
+        }
+    }else{
+        res.redirect("/login")
+    }
 })
 
 module.exports = router
