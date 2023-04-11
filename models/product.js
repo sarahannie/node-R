63c6be398f163ed28e1cd090c5e23bebb1a47772
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-04-11 09:01:25 +0100

    modify the model of the --roduct

--- models/product.js
+++ models/product.js
@@ -3,21 +3,29 @@ const mongoose = require("mongoose");
 const ProductItem = new mongoose.Schema({
     name:{
         type:String,
-        
+        required:true
     },
     description:{
         type:String,
+        required:true
         
     },
     quantity:{
         type:Number,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
     },
     price:{
         type:Number,
+        required:true
         
     },
     image:{
         type:String,
+        required:true
         
     }
 })
